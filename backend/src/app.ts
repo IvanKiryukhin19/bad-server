7083116199079c74a676f9774ffeed98fbfaccb6
IvanKiryukhin19 <kirivn@yandex.ru>
2025-09-27 12:14:42 +0300

    update cors, limiter, add sizeFile

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,35 +16,30 @@ const { PORT = 3000 } = process.env
 const app = express()
 
 app.use(cookieParser())
-export const limiter=rateLimit({
-    windowMs: 5 * 60 * 1000, // 15 minutes
-    //limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    //standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-    //legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-    //ipv6Subnet: 60, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-    max: 50,
-    statusCode: 429,
-    message: 'The request limit is reached.',
-})
-app.use(limiter)
 
-app.use(cors({
-  origin: 'http://localhost',
-  credentials: true
-}));
+app.use(
+    rateLimit({
+        windowMs: 5 * 60 * 1000,
+        max: 50,
+        statusCode: 429,
+        message: 'The request limit is reached.',
+        headers: true,
+    })
+)
 
-//app.use(cors())
-// app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    cors({
+        origin: ['http://localhost','http://localhost:3000','http://localhost:5173'],
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        methods: ['GET', 'POST', 'PATCH', 'DELETE'] 
+    })
+);
 
 app.use(serveStatic(path.join(__dirname, 'public')))
 app.use(json({ limit: '10mb' }))
 app.use(urlencoded({ extended: true, limit: '10mb' }))
 
-//app.use(urlencoded({ extended: true }))
-//app.use(json())
-
-//app.options('*', cors())
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express'
 import { uploadFile } from '../controllers/upload'
 import fileMiddleware from '../middlewares/file'
-import { checkMinSize } from '../middlewares/sizedata'
 
 const uploadRouter = Router()
-uploadRouter.post('/', fileMiddleware.single('file'), checkMinSize(2048),uploadFile)
+uploadRouter.post('/', fileMiddleware.single('file'), uploadFile)
 
 export default uploadRouter
